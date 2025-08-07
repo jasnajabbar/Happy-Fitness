1b2ad652a96b22b5dab1ccb14ece181df359a6e3
jasnajabbar <your-email@example.com>
2025-08-07 19:42:02 +0530

    changed cors setup

--- Backend/index.js
+++ Backend/index.js
@@ -11,11 +11,20 @@ const fitnessroutes=require('./src/routes/fitnessroutes');
 const app=express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://happy-fitness-fe.vercel.app'],
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like curl or Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
+
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); // Preflight
