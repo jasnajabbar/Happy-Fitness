6b23a4976e2f5263810e36854ad98c8bca425227
jasnajabbar <your-email@example.com>
2025-09-20 10:45:38 +0530

    env file change also cors issue fix

--- Backend/index.js
+++ Backend/index.js
@@ -38,7 +38,7 @@ const corsOptions = {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      console.error('❌ Blocked by CORS:', origin);
+      console.error('Blocked by CORS:', origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
