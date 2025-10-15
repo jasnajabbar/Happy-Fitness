c595a06a3579cd32af849df8352870bc98fc5b6a
jasnajabbar <your-email@example.com>
2025-10-15 18:10:25 +0530

    cors changes for single url

--- Backend/index.js
+++ Backend/index.js
@@ -8,30 +8,41 @@ const fitnessroutes = require('./src/routes/fitnessroutes');
 
 const app = express();
 
-const allowedOrigins = [process.env.ALLOWED_ORIGINS] //single origin in array
+const allowedOrigins =process.env.ALLOWED_ORIGINS;
  
-//CORS configuration
-const corsOptions = {
-  origin: function (origin,callback) { //origin - fe url , cb- be response Yes → callback(null, true)
-    // No → callback(new Error("Not allowed by CORS"))
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); //allow
-    } else {
-      console.error('Blocked by CORS:', origin);
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'], //options OPTIONS is an HTTP method, just like GET or POST.
-  // But it is not used to get or send data.
-  // Its main job is to ask the server “hey, is it OK if I send this request?”
-  // Think of it like asking permission first before doing something.
-  allowedHeaders: ['Content-Type','Authorization']
-};
+//CORS configuration for multiple url
+// const corsOptions = {
+//   origin: function (origin,callback) { //origin - fe url , cb- be response Yes → callback(null, true)
+//     // No → callback(new Error("Not allowed by CORS"))
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true); //allow
+//     } else {
+//       console.error('Blocked by CORS:', origin);
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'], //options OPTIONS is an HTTP method, just like GET or POST.
+//   // But it is not used to get or send data.
+//   // Its main job is to ask the server “hey, is it OK if I send this request?”
+//   // Think of it like asking permission first before doing something.
+//   allowedHeaders: ['Content-Type','Authorization']
+// };
 
 //Apply CORS before all routes/middleware
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+//single url
+app.use(cors({
+  origin: allowedOrigin,
+  credentials: true,
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
+app.options('*', cors({
+  origin: allowedOrigin,
+  credentials: true,
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
 
 //middleware
 app.use(bodyParser.json());
